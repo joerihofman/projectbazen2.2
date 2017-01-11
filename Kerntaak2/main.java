c2ad90b45fc706401d2021b8a8880e758dea8243
jorisjjansen <jorisjip@gmail.com>
2017-01-11 11:58:42 +0100

    zinder troep

--- Kerntaak2/main.java
+++ Kerntaak2/main.java
@@ -1,11 +1,9 @@
 import Kerntaak2.Connectie;
-//oee
+
 public class main {
 
     public static void main (String[] args) {
         connectie con = new Connectie();
         System.out.println("iets gebeurt");
-
-
     }
 }
