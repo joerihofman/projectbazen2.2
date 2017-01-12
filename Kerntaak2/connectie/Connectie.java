756bce071e325fe9c00ec1ffc97589397b55db48
qwerty12794 <onzin09@gmail.com>
2017-01-12 13:40:57 +0100

    Kerntaak2: XML READER staat nu in zijn geheel in de main

--- Kerntaak2/connectie/Connectie.java
+++ Kerntaak2/connectie/Connectie.java
@@ -1,11 +1,9 @@
-package connectie; /**
- * Created by MSI on 11-1-2017.
- */
+package connectie;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connectie {
-    Connection connectie = DriverManager.getConnection(localhost, root);
+//    Connection connectie = DriverManager.getConnection(null,null,null);
 }
