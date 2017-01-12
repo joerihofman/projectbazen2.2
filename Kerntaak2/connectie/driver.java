f59dd50e0d1a636be06f1968c80e55b64a850377
jorisjjansen <jorisjip@gmail.com>
2017-01-12 14:25:09 +0100

    connectie met de een database kan gemaakt worden met jdbc

--- Kerntaak2/connectie/driver.java
+++ Kerntaak2/connectie/driver.java
@@ -9,12 +9,15 @@ import java.sql.*;
 public class driver {
         public static void main(String[] args){
             try{
+                //Connection is made
                 Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/bier", "root", "");
 
                 Statement myStmt = myConn.createStatement();
 
+                //Query wordt gemaakt
                 ResultSet myRs = myStmt.executeQuery("select * from bier");
 
+                //Printen
                 while (myRs.next()) {
                     System.out.println(myRs.getFloat("alcohol"));
                 }
